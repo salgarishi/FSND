36ce4c1454aefe34ddd4b4d918a91f21f0c025dd
salgarishi <81615910+salgarishi@users.noreply.github.com>
2021-06-09 23:59:56 +0300

    Add files via upload

--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -1,5 +1,3 @@
-DATABASE_URL: postgres://sljqkkkxdpdbhd:eb297ded42440f1d7d711601a2533d66676b32a537657750188a1720c6bace10@ec2-18-214-195-34.compute-1.amazonaws.com:5432/d3vb1pi15q70cp
-HEROKU_POSTGRESQL_COBALT_URL: postgres://joeiqqqyetndqe:45ad39a89116cb2db7a3e15bed35f4c02f2095044e413f07b7efbe953c16de9b@ec2-52-86-2-228.compute-1.amazonaws.com:5432/d36kdacsl325nk
 curl --request POST \
   --url https://salgarishi.us.auth0.com/oauth/token \
   --header 'content-type: application/json' \
@@ -11,5 +9,5 @@ export ALGORITHEMS=['RS256']
 export ROWS_PER_PAGE = 10
 export database_name = "Agency"
 export database_path = "postgresql://postgres:123!@#@{}/{}".format('localhost:5432', database_name)
-export AUTH0_CALLBACK_URL='https://Salquraishi.herokuapp.com/'
+export AUTH0_CALLBACK_URL='https://salquraishi-agency.herokuapp.com/'
 
